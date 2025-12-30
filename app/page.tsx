c65e96152dc1d07da210befb87e64c7be0285b97
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2025-12-30 15:59:17 -0600

    home page in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import Image from "next/image";
 
 const Page = () => {
-    return <div></div>
+    return <main className="main-container">
+        <section className="home-grid">
+            <div id="coin-overview">
+                <div className="header">
+                    <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin Logo" width={56} height={56} />
+                </div>
+            </div>
+
+            <p>Trending Coins</p>
+        </section>
+
+        <section className="w-full mt-7 space-y-4">
+            <p>Categories</p>
+        </section>
+
+    </main>
 }
 export default Page
