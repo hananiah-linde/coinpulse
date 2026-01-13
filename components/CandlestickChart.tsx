01fffce0b988bf11d1eb91971e0cd64c661ba777
hananiah-linde <hananiah.linde@gmail.com>
2026-01-13 15:32:59 -0600

    update

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -1,6 +1,43 @@
-import React from 'react';
+'use client';
 
-const CandlestickChart = () => {
-  return <div>CandlestickChart</div>;
+import { useState } from 'react';
+import { PERIOD_BUTTONS } from '@/constants';
+
+const CandlestickChart = ({
+  children,
+  data,
+  coinId,
+  height = 360,
+  initalPeriod = 'daily',
+}: CandlestickChartProps) => {
+  const [loading, setLoading] = useState(false);
+  const [period, setPeriod] = useState(initalPeriod);
+  const handlePeriodChange = (newPeriod: Period) => {
+    if (newPeriod === period) return;
+
+    //TODO update period
+    setPeriod(newPeriod);
+  };
+  return (
+    <div id="candlestick-chart">
+      <div className="chart-header">
+        <div className="flex-1">{children}</div>
+
+        <div className="button-group">
+          <span className="text-sm mx-x font-medium text-purple-100/50">Period:</span>
+          {PERIOD_BUTTONS.map(({ value, label }) => (
+            <button
+              key={value}
+              className={period === value ? 'config-button-active' : 'config-button'}
+              onClick={() => handlePeriodChange(value)}
+              disabled={loading}
+            >
+              {label}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default CandlestickChart;
--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -3,11 +3,13 @@ import { fetcher } from '@/lib/coingecko.actions';
 import Image from 'next/image';
 import { formatCurrency } from '@/lib/utils';
 import { CoinOverviewFallback } from '@/components/home/fallback';
-import CandlestickChart from "@/components/CandlestickChart";
+import CandlestickChart from '@/components/CandlestickChart';
 
 const CoinOverview = async () => {
+  let coin;
+  let coinOHLCData;
   try {
-    const [coin, coinOHLCData] = await Promise.all([
+    [coin, coinOHLCData] = await Promise.all([
       fetcher<CoinDetailsData>('coins/bitcoin', {
         dex_pair_format: 'symbol',
       }),
@@ -18,11 +20,13 @@ const CoinOverview = async () => {
         precision: 'full',
       }),
     ]);
-
-    return (
-      <div id="coin-overview">
-        <CandlestickChart></CandlestickChart>
-
+  } catch (error) {
+    console.error('Failed to fetch coin overview:', error);
+    return <CoinOverviewFallback />;
+  }
+  return (
+    <div id="coin-overview">
+      <CandlestickChart data={coinOHLCData} coinId="bitcoin">
         <div className="header pt-2">
           <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
           <div className="info">
@@ -32,11 +36,8 @@ const CoinOverview = async () => {
             <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
           </div>
         </div>
-      </div>
-    );
-  } catch (error) {
-    console.error('Failed to fetch coin overview:', error);
-    return <CoinOverviewFallback />;
-  }
+      </CandlestickChart>
+    </div>
+  );
 };
 export default CoinOverview;
