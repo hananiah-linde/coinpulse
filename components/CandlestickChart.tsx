13b399cd774428b87620e5b4d958d594e9ae3274
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2026-01-13 15:57:21 -0600

    updates

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -1,23 +1,53 @@
 'use client';
 
-import { useState } from 'react';
-import { PERIOD_BUTTONS } from '@/constants';
+import {useRef, useState, useTransition} from 'react';
+import {PERIOD_BUTTONS, PERIOD_CONFIG} from '@/constants';
+import {IChartApi, ISeriesApi} from "lightweight-charts";
+import {fetcher} from "@/lib/coingecko.actions";
 
 const CandlestickChart = ({
   children,
   data,
   coinId,
   height = 360,
-  initalPeriod = 'daily',
+  initialPeriod = 'daily',
 }: CandlestickChartProps) => {
+  const chartContainerRef = useRef<HTMLDivElement | null>(null);
+  const chartRef = useRef<IChartApi | null>(null);
+  const candleSeriesRef = useRef<ISeriesApi<'Candlestick'> | null>(null);
+
   const [loading, setLoading] = useState(false);
-  const [period, setPeriod] = useState(initalPeriod);
+  const [period, setPeriod] = useState(initialPeriod);
+  const [ohlcData, setOhlcData] = useState<OHLCData[]>(data ?? []);
+  const [isPending, startTransition] = useTransition();
+
+  const fetchOhlcData = async (selectedPeriod: Period) => {
+    try {
+      const config = PERIOD_CONFIG[selectedPeriod];
+
+      const newData = await fetcher<OHLCData[]>(`coins/${coinId}/ohlc`, {
+        vs_currency: 'usd',
+        days: config.days,
+        //interval: config.interval, //for paid api only
+        precision: 'full',
+      })
+
+      setOhlcData(newData ?? []);
+    }
+    catch (error) {
+      console.error('Failed to fetch OHLC data:', error);
+    }
+  }
+
   const handlePeriodChange = (newPeriod: Period) => {
     if (newPeriod === period) return;
 
-    //TODO update period
-    setPeriod(newPeriod);
+    startTransition(async () => {
+      setPeriod(newPeriod);
+      await fetchOhlcData(newPeriod);
+    });
   };
+
   return (
     <div id="candlestick-chart">
       <div className="chart-header">
@@ -37,6 +67,7 @@ const CandlestickChart = ({
           ))}
         </div>
       </div>
+      <div ref={chartContainerRef} className="chart" style={{ height} }/>
     </div>
   );
 };
